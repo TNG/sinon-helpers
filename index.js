01ceb9bd24897683d706a66bc401fe8461079ff4
Lukas Taegert <lukas.taegert@tngtech.com>
2016-09-19 07:59:38 +0200

    feat(SpyConstructor): Add getSpyConstructor

--- index.js
+++ index.js
@@ -8,13 +8,26 @@ var setMethodToStub = R.curry(function (object, methodName) {
   object[ methodName ] = sinon.stub()
 })
 
-var stubMethodWithParams = R.curry(function (object, params) {
-  setMethodToStub(object, params.value)
+function configureStub (object, params) {
   if (params.hasOwnProperty(ARG_RETURN_THIS)) {
     object[ params.value ].returnsThis()
   } else if (params.hasOwnProperty(ARG_RETURN_VAL)) {
     object[ params.value ].returns(params[ ARG_RETURN_VAL ])
   }
+}
+var setMethodToStubWithParams = R.curry(function (object, params) {
+  setMethodToStub(object, params.value)
+  configureStub(object, params)
+})
+
+var stubMethodWithParams = R.curry(function (object, params) {
+  object[ params.value ] && object[ params.value ].restore && object[ params.value ].restore()
+  sinon.stub(object, params.value)
+  configureStub(object, params)
+})
+
+var spyOnMethod = R.curry(function (object, methodName) {
+  sinon.spy(object, methodName)
 })
 
 var applyToEachFunctionKeyInObject = function (appliedFunction, object) {
@@ -61,7 +74,7 @@ function getStubConstructor (Target) {
     instances.push(this)
 
     applyToEachFunctionKeyInPrototypeChain(setMethodToStub(this), Target.prototype)
-    stubParams.forEach(stubMethodWithParams(this))
+    stubParams.forEach(setMethodToStubWithParams(this))
   }
 
   function withMethods (methods) {
@@ -90,8 +103,59 @@ function getStubConstructor (Target) {
   return StubConstructor
 }
 
+function getConstructorInstanceWithArgsArray (Constructor, argsArray) {
+  function HelpConstructor (args) {
+    return Constructor.apply(this, args)
+  }
+
+  HelpConstructor.prototype = Constructor.prototype
+  return new HelpConstructor(argsArray)
+}
+
+function getSpyConstructor (Target) {
+  var instances = []
+  var instanceArgs = []
+  var stubParams = []
+
+  function SpyConstructor () {
+    var instance = getConstructorInstanceWithArgsArray(Target, arguments)
+    instanceArgs.push(getArrayFromArrayLikeObject(arguments))
+    instances.push(instance)
+
+    applyToEachFunctionKeyInPrototypeChain(spyOnMethod(instance), instance)
+    stubParams.forEach(stubMethodWithParams(instance))
+    return instance
+  }
+
+  function withStubs (stubs) {
+    stubParams = stubs
+    return this
+  }
+
+  SpyConstructor.withStubs = fa.createFunc(withStubs)
+
+  SpyConstructor.getInstances = function () {
+    return instances
+  }
+
+  SpyConstructor.getInstance = function (index) {
+    return instances[ getInstanceIndexWithValidation(index, instances.length) ]
+  }
+
+  SpyConstructor.getInstancesArgs = function () {
+    return instanceArgs
+  }
+
+  SpyConstructor.getInstanceArgs = function (index) {
+    return instanceArgs[ getInstanceIndexWithValidation(index, instances.length) ]
+  }
+
+  return SpyConstructor
+}
+
 module.exports = {
   getStubConstructor: getStubConstructor,
+  getSpyConstructor: getSpyConstructor,
   returning: fa.createArg({ args: [ ARG_RETURN_VAL ], extendsPrevious: true }),
   returningThis: fa.createArg({ extra: { returnThis: true }, extendsPrevious: true })
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,6 @@
 var sinonHelpers = require('../index')
 var getStubConstructor = sinonHelpers.getStubConstructor
+var getSpyConstructor = sinonHelpers.getSpyConstructor
 var returning = sinonHelpers.returning
 var returningThis = sinonHelpers.returningThis
 
@@ -162,6 +163,167 @@ describe('getStubConstructor', function () {
       new StubConstructor('foo', 'bar')
 
       expect(function () { StubConstructor.getInstanceArgs(1) }).to.throw(/1 instances/)
+    })
+  })
+})
+
+describe('getSpyConstructor', function () {
+  var TestConstructor, SpyConstructor
+
+  beforeEach(function () {
+    var testPrototype2 = {
+      proto2: function () { return 'p2' }
+    }
+    var testPrototype1 = Object.create(testPrototype2, {
+      proto1: { writable: true, enumerable: false, value: function () { return 'p1' } }
+    })
+    var testPrototype = Object.create(testPrototype1, {
+      field1: { writable: true, enumerable: true, value: function () { return 1 } },
+      field2: { writable: true, enumerable: false, value: function () { return 2 } }
+    })
+    TestConstructor = function () {
+      this.field3 = function () { return 3 }
+    }
+    TestConstructor.prototype = testPrototype
+    SpyConstructor = getSpyConstructor(TestConstructor)
+  })
+
+  it('should return a constructor that creates an object with all methods the constructor creates', function () {
+    var spiedObject = new SpyConstructor()
+
+    expect(spiedObject.field1()).to.equal(1)
+    expect(spiedObject.field2()).to.equal(2)
+    expect(spiedObject.field3()).to.equal(3)
+    expect(spiedObject.proto1()).to.equal('p1')
+    expect(spiedObject.proto2()).to.equal('p2')
+  })
+
+  it('should put spies on all methods', function () {
+    var spiedObject = new SpyConstructor()
+
+    expect(spiedObject.field1).to.have.property('isSinonProxy', true)
+    expect(spiedObject.field2).to.have.property('isSinonProxy', true)
+    expect(spiedObject.field3).to.have.property('isSinonProxy', true)
+    expect(spiedObject.proto1).to.have.property('isSinonProxy', true)
+    expect(spiedObject.proto2).to.have.property('isSinonProxy', true)
+  })
+
+  describe('withStubs', function () {
+    it('should allow stubbing methods', function () {
+      SpyConstructor = SpyConstructor.withStubs('field1', 'field3', 'proto1')
+      var spiedObject = new SpyConstructor()
+
+      expect(spiedObject.field1).to.have.property('isSinonProxy', true)
+      expect(spiedObject.field3).to.have.property('isSinonProxy', true)
+      expect(spiedObject.proto1).to.have.property('isSinonProxy', true)
+      expect(spiedObject.field1()).to.be.undefined
+      expect(spiedObject.field3()).to.be.undefined
+      expect(spiedObject.proto1()).to.be.undefined
+    })
+
+    it('should allow specifying stub return values', function () {
+      SpyConstructor = SpyConstructor.withStubs('field1', returning(10), 'field3', returning(20), 'proto1')
+      var spiedObject = new SpyConstructor()
+
+      expect(spiedObject.field1()).to.equal(10, 'field1')
+      expect(spiedObject.field3()).to.equal(20, 'field3')
+      expect(spiedObject.proto1()).to.be.undefined
+    })
+
+    it('should allow for methods to return their this value', function () {
+      SpyConstructor = SpyConstructor.withStubs('field1', returningThis, 'field3', returningThis, 'proto1')
+      var spiedObject = new SpyConstructor()
+
+      expect(spiedObject.field1().field3().proto1).to.be.a('function')
+    })
+  })
+
+  describe('getInstances', function () {
+    it('should return an empty list if there are no instances', function () {
+      expect(SpyConstructor.getInstances()).to.deep.equal([])
+    })
+
+    it('should return a list of instances', function () {
+      var instance1 = new SpyConstructor()
+      var instance2 = new SpyConstructor()
+
+      expect(SpyConstructor.getInstances()).to.deep.equal([ instance1, instance2 ])
+    })
+  })
+
+  describe('getInstance', function () {
+    it('should return a single instance if one has been created', function () {
+      var instance = new SpyConstructor()
+
+      expect(SpyConstructor.getInstance()).to.equal(instance)
+    })
+
+    it('should throw an error if no instance has been created', function () {
+      expect(SpyConstructor.getInstance).to.throw(/0 instances/)
+    })
+
+    it('should throw an error if more than one instance has been created', function () {
+      new SpyConstructor()
+      new SpyConstructor()
+
+      expect(SpyConstructor.getInstance).to.throw(/2 instances/)
+    })
+
+    it('should return an instance with a given index', function () {
+      new SpyConstructor()
+      var instance2 = new SpyConstructor()
+
+      expect(SpyConstructor.getInstance(1)).to.equal(instance2)
+    })
+
+    it('should throw an error if not enough instances exist', function () {
+      new SpyConstructor()
+
+      expect(function () { SpyConstructor.getInstance(1) }).to.throw(/1 instances/)
+    })
+  })
+
+  describe('getInstancesArgs', function () {
+    it('should return an empty list if there are no instances', function () {
+      expect(SpyConstructor.getInstancesArgs()).to.deep.equal([])
+    })
+
+    it('should return a list of constructor arguments', function () {
+      new SpyConstructor('foo', 'bar')
+      new SpyConstructor('baz', 'bla')
+
+      expect(SpyConstructor.getInstancesArgs()).to.deep.equal([ [ 'foo', 'bar' ], [ 'baz', 'bla' ] ])
+    })
+  })
+
+  describe('getInstanceArgs', function () {
+    it('should return the arguments of a single instance if one has been created', function () {
+      new SpyConstructor('foo', 'bar')
+      expect(SpyConstructor.getInstanceArgs()).to.deep.equal([ 'foo', 'bar' ])
+    })
+
+    it('should throw an error if no instance has been created', function () {
+      expect(SpyConstructor.getInstanceArgs).to.throw(/0 instances/)
+    })
+
+    it('should throw an error if more than one instance has been created', function () {
+      new SpyConstructor('foo', 'bar')
+      new SpyConstructor('baz', 'bla')
+
+      expect(SpyConstructor.getInstanceArgs).to.throw(/2 instances/)
+    })
+
+    it('should return the arguments of an instance with a given index', function () {
+      new SpyConstructor('foo', 'bar')
+      new SpyConstructor('baz', 'bla')
+
+      expect(SpyConstructor.getInstanceArgs(1)).to.deep.equal([ 'baz', 'bla' ])
+    })
+
+    it('should throw an error if not enough instances exist', function () {
+      new SpyConstructor('foo', 'bar')
+
+      expect(function () { SpyConstructor.getInstanceArgs(1) }).to.throw(/1 instances/)
     })
   })
 })
